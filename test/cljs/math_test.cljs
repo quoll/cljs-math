78ee419e6ead173569bbfeffc9168d2fd244487c
Paula Gearon <pgearon@cisco.com>
2021-12-03 15:27:52 -0500

    Added negative number test to fmod

--- test/cljs/math_test.cljs
+++ test/cljs/math_test.cljs
@@ -18,11 +18,14 @@
 (deftest test-fmod
   (testing "The fmod function"
     (let [a (IEEE-fmod 5.2 2.3)
-          b (IEEE-fmod 339794.000868 1.69897000343)]
+          b (IEEE-fmod 339794.000868 1.69897000343)
+          c (IEEE-fmod -339794.000868 1.69897000343)]
       (is (= 0.60000000000000053291 a))
       (is (= "3fe3333333333338" (buffer-str a)))
       (is (= 0.00018199998751811108 b))
-      (is (= "3f27dae7fd100000" (buffer-str b))))))
+      (is (= "3f27dae7fd100000" (buffer-str b)))
+      (is (= -0.00018199998751811108 c))
+      (is (= "bf27dae7fd100000" (buffer-str c))))))
 
 (deftest test-remainder
   (testing "The JS remainder function"
